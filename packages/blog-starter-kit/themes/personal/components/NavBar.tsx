a2fe8a293ac14dafafd0bf23eb5f0a90bb842f23
sayaanalam <31066332+sayaanalam@users.noreply.github.com>
2024-05-20 15:22:48 +0530

    Update NavBar.tsx

--- packages/blog-starter-kit/themes/personal/components/NavBar.tsx
+++ packages/blog-starter-kit/themes/personal/components/NavBar.tsx
@@ -76,6 +76,7 @@ const MobileNavItem = ({ href, children }: React.PropsWithChildren<{ href: strin
 const DesktopNavigation = () => {
   return (
     <nav className="flex justify-center">
+      <div className="max-w-4xl w-full px-1"> {/* Adjust the max-width to your desired width */}
         <ul className="flex rounded-full bg-white/90 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
           {NavigationItems.map((item) => {
             if (item.type === 'internal') {
