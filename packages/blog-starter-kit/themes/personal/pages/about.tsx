092663b34f1353ba168a00cfcaf712b7fafb519d
sayaanalam <31066332+sayaanalam@users.noreply.github.com>
2024-05-20 13:13:46 +0530

    Create about.tsx

---
+++ packages/blog-starter-kit/themes/personal/pages/about.tsx
@@ -0,0 +1,14 @@
+// pages/about.tsx
+
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1>About Me</h1>
+      <p>This is the about page.</p>
+    </div>
+  );
+};
+
+export default About;
